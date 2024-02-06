de3e5c0847ea3cbb791c5c5242059701a0c44ded
Alex Amauri <alex.mauri.mc@gmail.com>
2024-02-06 15:27:03 -0600

    CRUD in the API

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,9 +1,46 @@
-const productsController = {};
+import productDAO from "../dao/products.dao.js";
 
-productsController.getAll = (req,res) =>{
-    res.json({
-        status: "hola"
-    })
+export const getAll = (req,res) =>{
+
+    productDAO.getAll()
+    .then(result=> res.json(result))
+    .catch(err=>res.json({
+        status: "Server Unavailable"
+    }));
 }
 
-export default productsController;
+export const getOne = (req, res) => {
+    const barcode = req.params.barcode;
+    productDAO.getOne(barcode)
+        .then(result => {
+            !result ? res.json({ message: "Product not found" }) : res.json(result);
+        })
+        .catch(err => res.json({
+            status: "Server Unavailable"
+        }));
+};
+
+
+export const insertOne = (req, res) => {
+    productDAO.insertOne(req.body)
+        .then(result => {
+            res.json(!result ? { status: 'No se guardo el producto' } : { estatus: 'Producto insertado', product: result });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ status: 'Error en el servidor' });
+        });
+  };
+
+
+export const updateOne = (req, res) => {
+    productDAO.updateOne(req.params.barcode,req.body)
+    .then(result=>res.json({status: "Product Updated"}))
+    .catch(err=>res.json({status: "Server Unavaible"}));
+}
+
+export const deleteOne = (req, res) => {
+    productDAO.deleteOne(req.params.barcode)
+    .then(result=>res.json({status: "Boleto Borrado" }))
+    .catch(err=>res.json({status: "Servidor no responde"}));
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import productsController from "../controllers/products.controller.js";
+import {getAll, getOne, insertOne, updateOne, deleteOne} from "../controllers/products.controller.js";
 
 const router = Router();
 
-router.get('/getAll', productsController.getAll);
+router.get('/getAll', getAll);
 
+router.get('/getOne/:barcode', getOne);
 
+router.post('/insertOne', insertOne);
 
+router.put('/updateOne/:barcode', updateOne);
+
+router.delete('/deleteOne/:barcode', deleteOne);
 
 export default router;
